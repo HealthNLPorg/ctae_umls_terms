6549dc2e8cf528d76eba70bff0eb8ba99b2ed4dc
Eli Goldner <etgld@posteo.us>
2025-08-28 15:53:12 -0700

    Draft finished

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import requests
 import argparse
 import logging
 from collections.abc import Iterable
-from functools import lru_cache
+from itertools import chain
+from functools import lru_cache, partial
 import json
 
 # import csv
-from requests.exceptions import HTTPError
 
 from umls_python_client import UMLSClient
 import os
@@ -52,6 +52,18 @@ parser.add_argument(
     default="CuiSynonyms.csv",
     help="Destination filename for CUI names",
 )
+parser.add_argument(
+    "--page_size",
+    type=int,
+    default=1_000,
+    help="Need to check what this does",
+)
+parser.add_argument(
+    "--lowercase_synonyms",
+    type=bool,
+    default=True,
+    help="Whether to normalize synonyms by lowercasing",
+)
 logger = logging.getLogger(__name__)
 
 logging.basicConfig(
@@ -61,92 +73,65 @@ logging.basicConfig(
 )
 
 
-def collect_descendant_cuis(url: str, api_key: str, page_size: int) -> set[str]:
-    """
-    Collect cuis for descendants.
-    - Parameters:
-        url (str): The url to use to fetch descendants.
-    - Returns:
-        set: set of unique descendant cuis.
-    """
+@lru_cache
+def collect_descendant_cuis(api_key: str, page_size: int, url: str) -> Iterable[str]:
     resp = requests.get(
         url,
         params={
             "apiKey": api_key,
             "language": "ENG",
-            #            "pageNumber": page,
             "pageSize": page_size,
-            #            "includeObsolete": False,
-            #            "includeSuppressible": False
         },
     )
     resp.raise_for_status()
-    desc_cuis = set()
     for atom in resp.json().get("result"):
         desc_concept = atom.get("concept")
         desc_url_path = desc_concept.split("/")
         desc_cui = desc_url_path[-1]
-        desc_cuis.add(desc_cui)
-    return desc_cuis
+        yield desc_cui
+
 
 @lru_cache
 def get_cui_atoms_dict(cui_api, cui: str) -> dict:
     return json.loads(cui_api.get_atoms(cui=cui, language="ENG"))
 
 
-# cui_atoms = cui_api.get_atoms(cui=cui, language="ENG")
-# atoms_dict = json.loads(cui_atoms)
-def collect_cuis(atoms_dict: dict[str, dict[str, str]]) -> set[str]:
-    all_desc_cuis = set()
+def collect_cuis(cui_api, api_key: str, page_size: int, cui: str) -> Iterable[str]:
+    atoms_dict = get_cui_atoms_dict(cui_api, cui)
     for atom in atoms_dict.get("result"):
         if atom is not None:
             descendants_url = atom.get("descendants")
             if descendants_url is not None and descendants_url != "NONE":
-                desc_cuis = collect_descendant_cuis(descendants_url)
-                all_desc_cuis.update(desc_cuis)
-    return all_desc_cuis
+                yield from collect_descendant_cuis(descendants_url, api_key, page_size)
 
 
-def get_cui_synonymns(
-    atoms_dict: dict[str, dict[str, str]], lowercase_synonyms: bool
-) -> set[str]:
-    synonyms = set()
+def atom_to_synonym(lowercase_synonym: bool, atom: dict) -> str | None:
+    tt = atom.get("termType")
+    if tt == "FN" or tt == "OF" or tt == "OAF" or tt == "OAP":
+        return None
+    return atom.get("name").lower() if lowercase_synonym else atom.get("name")
+
+
+def get_cui_synonymns(cui_api, lowercase_synonyms: bool, cui: str) -> Iterable[str]:
+    atoms_dict = get_cui_atoms_dict(cui_api, cui)
+    get_synonym = partial(atom_to_synonym, lowercase_synonyms)
     for atom in atoms_dict.get("result"):
-        #        print(atom)
-        tt = atom.get("termType")
-        if tt == "FN" or tt == "OF" or tt == "OAF" or tt == "OAP":
-            # certain UMLS terms are a -Functional- type, which isn't a synonym.
-            continue
-        if lowercase_synonyms:
-            synonyms.add(atom.get("name").lower())
-        else:
-            synonyms.add(atom.get("name"))
-    return synonyms
+        synonym = get_synonym(atom)
+        if synonym is not None:
+            yield synonym
 
 
-# """
-# Save all synonym information to file.
-# - Parameters:
-#     cui (str): The UMLS Concept Unique Identifier.
-# """
-# cui_atoms = cui_api.get_atoms(cui=cui, language="ENG")
-# atoms_dict = json.loads(cui_atoms)
-def save_cui_synonyms(
+def save_cui_synonym_to_table(
     cui: str,
-    synonyms: Iterable[str],
+    unique_synonyms: Iterable[str],
     cui_synonym_path: str,
 ) -> None:
     with open(cui_synonym_path, "a") as f:
-        for synonym in synonyms:
+        for synonym in unique_synonyms:
             f.write(f"{cui},{synonym}\n")
 
 
-def save_cui_name(cui_api, cui: str, cui_name_path: str) -> None:
-    """
-    Save basic cui information to file.
-    - Parameters:
-        cui (str): The UMLS Concept Unique Identifier.
-    """
+def save_cui_name_to_table(cui_api, cui_name_path: str, cui: str) -> None:
     cui_info = cui_api.get_cui_info(cui=cui)
     cui_dict = json.loads(cui_info)
     cui_name = cui_dict.get("result").get("name")
@@ -161,55 +146,51 @@ def process(
     root_cui_fn: str,
     cui_name_fn: str,
     cui_synonym_fn: str,
+    page_size: int,
     lowercase_synonyms: bool,
-
 ) -> None:
-    # TODO migrate these and other global or
-    # hardcoded variables to function arguments
-    # and from there to cli options
-    # api_key = "6e862256-725b-4400-b9f7-8b6ae7e371a0"
-    # root_cui_fn = "RootCuis.txt"
-    # cui_name_fn = "CuiNames.csv"
-    # cui_synonym_fn = "CuiSynonyms.csv"
-
     root_cui_path = os.path.join(source_dir, root_cui_fn)
-    cui_name_path = os.path.join(source_dir, cui_name_fn)
-    cui_synonym_path = os.path.join(source_dir, cui_synonym_fn)
-    logger.info(f"Working Directory: {source_dir}")
+    cui_name_path = os.path.join(target_dir, cui_name_fn)
+    cui_synonym_path = os.path.join(target_dir, cui_synonym_fn)
+    logger.info(f"Working directory: {source_dir}")
     if os.path.exists(cui_name_path):
-        logger.info("Erasing Cui Name File:", cui_name_path)
+        logger.info(f"Erasing CUI name file: {cui_name_path}")
         os.remove(cui_name_path)
     if os.path.exists(cui_synonym_path):
-        logger.info("Erasing Cui Synonym File:", cui_synonym_path)
+        logger.info(f"Erasing CUI synonym file: {cui_synonym_path}")
         os.remove(cui_synonym_path)
 
-    # Initialize the CUIAPI class with your API key.
-    # TODO - retain in case we need to extend
-    # cui_api = client.cuiAPI
-    # source_api = client.sourceAPI
-    # Initialize the uml-python-client main client.
-    # client = UMLSClient(api_key=api_key)
-    # For case-sensitivity, set LOWERCASE_SYNONYMS = False
-    # LOWERCASE_SYNONYMS = True
-    # PAGE_SIZE = 1000
+    client = UMLSClient(api_key=api_key)
+    cui_api = client.cuiAPI
 
     logger.info(f"Reading Root CUIs from: {root_cui_path}")
     with open(root_cui_path, "r") as f:
-        root_cuis = set(line.strip() for line in f)
+        raw_root_cuis = filter(None, map(str.strip, f))
 
-    # Collect all descendant cuis for the root cuis.
-    all_cuis = set()
-    for root_cui in root_cuis:
-        all_cuis.add(root_cui)
-        desc_cuis = collect_cuis(root_cui)
-        all_cuis.update(desc_cuis)
+    local_collect_cuis = partial(collect_cuis, cui_api, api_key, page_size)
+    all_unique_cuis = sorted(
+        set(
+            chain.from_iterable(
+                chain((root_cui,), local_collect_cuis(root_cui))
+                for root_cui in raw_root_cuis
+            )
+        )
+    )
 
-    # Save all cuis and their synonyms to file.
-    for cui in all_cuis:
-        save_cui_name(cui)
-        synonyms = get_cui_synonymns()
-        save_cui_synonyms(
-            cui,
+    local_save_cui_name_to_table = partial(
+        save_cui_name_to_table,
+        cui_api,
+        cui_name_path,
+    )
+    local_get_cui_synonyms = partial(
+        get_cui_synonymns,
+        cui_api,
+        lowercase_synonyms,
+    )
+    for cui in all_unique_cuis:
+        local_save_cui_name_to_table(cui)
+        save_cui_synonym_to_table(
+            cui, sorted(set(local_get_cui_synonyms(cui))), cui_synonym_path
         )
 
 
@@ -222,6 +203,8 @@ def main():
         args.root_cui_fn,
         args.cui_name_fn,
         args.cui_synonym_fn,
+        args.page_size,
+        args.lowercase_synonyms,
     )
 
 
