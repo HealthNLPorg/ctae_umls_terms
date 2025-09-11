f7ec5dd4b9e5135f073c2f56a1554885c51d015e
Eli Goldner <etgld@posteo.us>
2025-09-11 11:31:16 -0700

    Added function for building CUI to event name mappings, some other rearchitecting

--- create_ctakes_bsv_and_cui_mappings.py
+++ create_ctakes_bsv_and_cui_mappings.py
@@ -2,16 +2,16 @@ import polars as pl
 import argparse
 import json
 from functools import partial
-import pathlib
 
 import os
 
 parser = argparse.ArgumentParser(description="")
 
 parser.add_argument(
-    "--input_csv",
+    "--input_csvs",
+    nargs="+",
     type=str,
-    help="GPT enriched synonym CSV for conversion",
+    help="GPT enriched synonym CSVs for conversion",
 )
 parser.add_argument(
     "--output_dir",
@@ -32,8 +32,9 @@ def parse_term_json(
     return term_dictionary.get(term_key)
 
 
-def create_ctakes_bsv(input_csv: str, output_dir: str, term_key: str = "term") -> None:
-    df = pl.read_csv(input_csv)
+def create_ctakes_bsv(
+    df: pl.DataFrame, output_dir: str, term_key: str = "term"
+) -> None:
     local_parse_term = partial(parse_term_json, term_key)
     cleaned_terms_df = df.with_columns(
         pl.col(term_key).map_elements(local_parse_term).alias(term_key)
@@ -45,15 +46,29 @@ def create_ctakes_bsv(input_csv: str, output_dir: str, term_key: str = "term") -
     cleaned_terms_df.with_columns(
         pl.col(term_key).map_elements(normalize_term).alias(term_key)
     ).write_csv(
-        os.path.join(output_dir, f"{pathlib.Path(input_csv)}.bsv"),
+        os.path.join(output_dir, "total.bsv"),
         separator="|",
+        include_header=False,
+    )
+
+
+def create_cui_mappings(df: pl.DataFrame, output_dir: str) -> None:
+    def normalize_term(term: str) -> str:
+        return " ".join(term.split()).title()
+
+    df.with_columns(pl.col("ctae").map_elements(normalize_term).alias("ctae")).select(
+        "root-cui", "ctae"
+    ).unique().write_csv(
+        os.path.join(output_dir, "cui_to_event_type.tsv"),
+        separator="\t",
         include_header=False,
     )
 
 
 def main() -> None:
     args = parser.parse_args()
-    create_ctakes_bsv(args.input_csv, args.output_dir)
+    df = pl.concat(pl.read_csv(input_csv) for input_csv in args.input_csvs)
+    create_ctakes_bsv(df, args.output_dir)
 
 
 if __name__ == "__main__":
