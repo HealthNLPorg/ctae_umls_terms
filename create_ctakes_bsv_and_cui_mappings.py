4c14afbb5af2893d44abd6099be08d7310754e89
Eli Goldner <etgld@posteo.us>
2025-10-07 17:51:11 -0400

    Update to CUI output format

--- create_ctakes_bsv_and_cui_mappings.py
+++ create_ctakes_bsv_and_cui_mappings.py
@@ -2,6 +2,7 @@ import polars as pl
 import argparse
 import json
 from functools import partial
+from typing import cast
 
 import os
 
@@ -29,7 +30,11 @@ def parse_term_json(
         term_dictionary = json.loads(term_json)
     except Exception:
         term_dictionary = {}
-    return term_dictionary.get(term_key)
+    term_value = term_dictionary.get(term_key)
+    if term_value is not None:
+        return term_value
+    ValueError(f"Empty term for {term_key} in {term_json}")
+    return ""
 
 
 def create_ctakes_bsv(
@@ -55,15 +60,27 @@ def create_ctakes_bsv(
 
 def create_cui_mappings(df: pl.DataFrame, output_dir: str) -> None:
     def normalize_term(term: str) -> str:
-        return " ".join(term.split()).title()
+        return " ".join(term.strip().split()).title()
 
-    df.with_columns(pl.col("ctae").map_elements(normalize_term).alias("ctae")).select(
-        "root-cui", "ctae"
-    ).unique().write_csv(
-        os.path.join(output_dir, "cui_to_event_type.tsv"),
-        separator="\t",
-        include_header=False,
+    def is_rt(normalized_term: str) -> bool:
+        return normalized_term == "Radiation Therapy (Procedure)"
+
+    cui_to_event_type = (
+        df.with_columns(pl.col("ctae").map_elements(normalize_term).alias("event_type"))
+        .select("root-cui", "event_type")
+        .unique()
     )
+
+    def linify(s: str) -> str:
+        return s + "\n"
+
+    for rt, group in cui_to_event_type.group_by(is_rt(cast(str, pl.col("event_type")))):
+        (rt,) = rt
+        cuis = group.select("root-cui").unique().to_series()
+        with open(
+            os.path.join(output_dir, "rt_cuis.txt" if rt else "ctae_cuis.txt"), mode="w"
+        ) as f:
+            f.writelines(map(linify, cuis))
 
 
 def main() -> None:
